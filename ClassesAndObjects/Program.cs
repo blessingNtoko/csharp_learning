6ed6ea1a6bef143edcfb6eacb875260dd7b57ea3
Gundo Ntoko <gundo@purplecosmos.co.za>
2022-09-16 10:11:03 +0200

    classes and objects

---
+++ ClassesAndObjects/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book book1 = new("The Deathly Hallows", "Rowling", 659);
+
+            Book book2 = new("Fellowship of the Ring", "Tolkien", 750);
+
+            // Console.WriteLine(book1.title);
+
+            Student student1 = new Student("Bob", "Engineering", 3.4);
+            Student student2 = new Student("Sarah", "Programming", 3.0);
+
+            Console.WriteLine(student1.HasHonours());
+
+        }
+    }
+}
---
+++ ClassesAndObjects/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    internal class Student
+    {
+        public string studentName;
+        public string course;
+        public double gpa;
+
+        public Student(string studentName, string course, double gpa)
+        {
+            this.studentName = studentName;
+            this.course = course;
+            this.gpa = gpa;
+        }   
+
+        public bool HasHonours()
+        {
+            if (this.gpa > 3.5)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
