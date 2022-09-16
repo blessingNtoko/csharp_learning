42b542f4ec2da0c95bc64188bc43055964a90610
Gundo Ntoko <gundo@purplecosmos.co.za>
2022-09-16 11:29:46 +0200

    work

---
+++ StaticClassAttributes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClassAttributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Song longaConversa = new("Longa Conversa", "Dj Danifox", 500);
+            Console.WriteLine(Song.songCount);
+            Song streets = new("Streets (Amapiano Remix)", "Loxiie Dee", 500);
+
+            Console.WriteLine(Song.songCount);
+        }
+    }
+}
